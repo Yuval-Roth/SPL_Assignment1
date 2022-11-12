900fa08bfac9a2aa5c97d0acb320360259f85041
Yuval-Roth <rothyuv@post.bgu.ac.il>
2022-11-12 23:00:25 +0200

    implemented Coalition addParty,flagAsProposed, checkIfAlreadyProposed methods

--- src/Coalition.cpp
+++ src/Coalition.cpp
@@ -1,17 +1,32 @@
 #include "../include/Coalition.h"
 
-const bool Coalition::checkIfAlreadyProposed(int PartyId)
+const bool Coalition::checkIfAlreadyProposed(int partyId)
 {
+    std::_List_iterator<int> iter ;
+    for (iter = Already_Proposed.begin() ; iter != Already_Proposed.end() ; iter++)
+    {
+        if(*iter == partyId) return true;
+    } 
+    return false;
+}
 
-    
-    // TODO : Complete this implementation
-    return true;
-
+void Coalition::flagAsProposed(int partyId)
+{
+    Already_Proposed.push_front(partyId);
 }
 
 void Coalition::addParty(int partyId)
 {
     Members.push_front(partyId);
+    std::_List_iterator<int> iter ;
+    for (iter = Already_Proposed.begin() ; iter != Already_Proposed.end() ; iter++)
+    {
+        if(*iter == partyId)
+         {
+          Already_Proposed.erase(iter);  
+          break;
+         }  
+    } 
 }
 
 const list<int>& Coalition::getMembers() const
